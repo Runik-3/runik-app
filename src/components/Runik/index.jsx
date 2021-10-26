49d5f64f2bcdcd670d0f38eb409d3332dd7ebd85
Jamie Samuel <jsam07@hotmail.com>
2021-10-26 16:43:07 -0700

    Delayed font on initial loading to avoid FOUC

--- src/components/Runik/index.jsx
+++ src/components/Runik/index.jsx
@@ -3,11 +3,11 @@ import { motion } from 'framer-motion';
 export default function Runik() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <div className="font-norse text-5xl w-auto">
+            <div className="font-norse text-5xl">
                 <motion.div
                     initial={{ y: 100, x: 50, opacity: 0, scale: 0 }}
                     animate={{ y: 0, x: 0, opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5, type: 'tween' }}
+                    transition={{ delay: 0.5, duration: 0.7, type: 'tween' }}
                 >
                     Welcome to Runik
                 </motion.div>
