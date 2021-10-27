40e56bb9529a7e6b3e525eb1bfe6c06364713338
Jamie Samuel <jsam07@hotmail.com>
2021-10-26 17:26:33 -0700

    Increased 'Welcome to Runik' font

--- src/components/Runik/index.jsx
+++ src/components/Runik/index.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 export default function Runik() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <div className="font-norse text-5xl">
+            <div className="font-norse text-8xl">
                 <motion.div
                     initial={{ y: 100, x: 50, opacity: 0, scale: 0 }}
                     animate={{ y: 0, x: 0, opacity: 1, scale: 1 }}
