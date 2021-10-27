c0e8668ea5f217af2fb53578f57c853f2ca78b09
dawsonGosse <dgosse5@my.bcit.ca>
2021-10-27 11:14:53 -0700

    created logo component

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/prop-types */
 import '../styles/globals.css';
 
-
+// this is a test comment
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
