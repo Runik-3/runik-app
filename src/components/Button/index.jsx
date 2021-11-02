e20573954daafe735fd19f843c90f44886aff16d
dawsonGosse <dgosse5@my.bcit.ca>
2021-11-01 19:20:10 -0700

    Created Button Comp

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,14 @@
+const Button = ({size}) => {
+    return (
+        <button
+            type="button"
+            className={`flex ${
+                size === 'md' ? 'py-6' : 'py-8'
+            } px-14 border-2 border-runik-neutral-dark rounded-2xl font-spartan text-2xl font-semibold`}
+        >
+            Download
+        </button>
+    );
+};
+
+export default Button;
