eb31d579c2899955c1130fbb9d589966ac7a047b
iflinda <informlinda@gmail.com>
2021-11-10 04:30:46 -0800

    Fixed color of the background slider

---
+++ src/components/Icons/Divider/index.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+const Divider = () => {
+    return (
+        <svg
+            width="287"
+            height="2"
+            viewBox="0 0 287 2"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <line
+                x1="-8.74228e-08"
+                y1="1"
+                x2="287"
+                y2="0.999975"
+                stroke="#808080"
+                strokeWidth="2"
+            />
+        </svg>
+    );
+};
+
+export default Divider;
