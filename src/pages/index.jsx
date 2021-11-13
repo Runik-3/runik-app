0263e8c73c958d0f9eacad6f3d9881b52024ceb2
EliasMatteo <eflamencosolito@my.bcit.ca>
2021-11-13 00:06:55 -0800

    additional fixes to pages that add to previous branch
    fixed navBar so now library comp pops up on all pages but in doing so its styling needs to be fixed now

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,12 +5,13 @@ import SearchBar from '../components/SearchBar';
 export default function Home() {
     return (
         <div className="flex flex-col min-h-full h-screen">
-            <NavBar visibility="hide" />
+            <NavBar visibility="invisible" aDisplay="flex" rDisplay="hidden" />
             <div className="flex flex-col items-center justify-between h-4/5 w-5/6 max-w-5xl mx-auto pt-20">
                 <MainLogo />
                 <SearchBar />
             </div>
             <div className=" flex bg-gradient-to-t from-gray-400 h-3/5" />
+            {/* #9ca3af */}
         </div>
     );
 }
--- src/pages/results.jsx
+++ src/pages/results.jsx
@@ -1,15 +1,20 @@
-import RunikIcon from '../components/Icons/RunikIcon/index';
-import LibraryIcon from '../components/Icons/LibraryIcon/index';
-import SearchBar from '../components/SearchBar';
+// import RunikIcon from '../components/Icons/RunikIcon/index';
+// import LibraryIcon from '../components/Icons/LibraryIcon/index';
+// import SearchBar from '../components/SearchBar';
 import ResultsCard from '../components/ResultsCard';
+import NavBar from '../components/NavBar';
+import ArrowBackIcon from '../components/Icons/ArrowBackIcon/Index';
 
 export default function Results() {
     return (
         <div className="flex justify-center w-full flex-col items-center">
+            {/* The NavBar has replaced the original standalone comps, the sizing and fitting is off and needs to be adjusted accordingly, the props attached to NavBar are for the icons that need to switch visibility depending on page */}
             <div className="flex py-5 w-11/12 max-w-8xl justify-center items-center">
-                <RunikIcon />
-                <SearchBar />
-                <LibraryIcon />
+                <NavBar aDisplay="hidden" rDisplay="flex" />
+            </div>
+            {/* this is new and needs to be here so user can go back to search page from results page, please make it fit correctly within the page */}
+            <div className="pt-8 flex justify-start items-start max-w-full">
+                <ArrowBackIcon path="/" />
             </div>
             <div />
             <div className="flex w-full max-w-6xl justify-evenly items-center py-16 flex-wrap">
