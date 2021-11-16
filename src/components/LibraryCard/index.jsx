b75b56d6d78e80018d2f4cd83f90ffd87b2f5e4f
iflinda <informlinda@gmail.com>
2021-11-16 05:59:48 -0800

    Fixed restructuring

--- src/components/LibraryCard/index.jsx
+++ src/components/LibraryCard/index.jsx
@@ -1,7 +1,7 @@
 import AddButton from '../Icons/AddButton';
 
-export default function LibraryCard(props) {
-    const { title, thumbnail } = { props };
+export default function LibraryCard(...props) {
+    const { title, thumbnail } = { ...props };
     return (
         <div className=" h-screen ">
             <div className="overflow-hidden flex p-3 pb-8 pt-2 shadow-lg rounded-lg w-80 h-20 bg-gradient-to-r from-runik-neutral-dark to-transparent justify-between background-image:">
