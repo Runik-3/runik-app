c3eebfc8e44f92c8365d500d02dc6c41f9569d8a
matteomiceli <omatteomiceli@gmail.com>
2021-11-16 03:08:14 -0800

    added author to fusejs search params

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -7,7 +7,7 @@ import SearchBarDropdown from '../SearchBarDropdown';
 import booksList from '../../data/booksList.json';
 
 const options = {
-    keys: ['title'],
+    keys: ['title', 'author'],
 };
 const fuse = new Fuse(booksList, options);
 
