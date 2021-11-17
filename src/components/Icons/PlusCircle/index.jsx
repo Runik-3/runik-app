15214e218cc34624bf6a9251e35f1593db0ba362
iflinda <informlinda@gmail.com>
2021-11-16 18:12:12 -0800

    Merged with feature/fsaccess

--- src/components/Icons/PlusCircle/index.jsx
+++ src/components/Icons/PlusCircle/index.jsx
@@ -3,7 +3,16 @@ import React, { useContext } from 'react';
 import booksList from '../../../data/booksList.json';
 import { LibraryContext } from '../../../context/libraryContext';
 
-export default function PlusCircle({ url, onclick }) {
+export default function PlusCircle({ url }) {
+    const [, addReference] = useContext(LibraryContext);
+
+    function addToLibrary() {
+        const bookToAdd = booksList.filter((book) => {
+            return book.url === url;
+        });
+        addReference(bookToAdd);
+    }
+
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -15,7 +24,7 @@ export default function PlusCircle({ url, onclick }) {
             strokeLinecap="round"
             strokeLinejoin="round"
             className="prefix__feather prefix__feather-plus-circle cursor-pointer hover:scale-110 transition-all"
-            onClick={() => onclick(url)}
+            onClick={() => addToLibrary(url)}
         >
             <circle cx={12} cy={12} r={10} />
             <path d="M12 8v8M8 12h8" />
