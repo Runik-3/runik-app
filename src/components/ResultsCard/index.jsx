ae2dc0ccd046730f5a42ca9171aae9e109fc08f8
iflinda <informlinda@gmail.com>
2021-11-23 23:30:56 -0800

    Made results card clickable and redirects the user to the details page. In the final version, an image map will have to be created to prevent user from being able to click on the entirity of the image.

--- src/components/ResultsCard/index.jsx
+++ src/components/ResultsCard/index.jsx
@@ -35,27 +35,27 @@ export default function ResultsCard({ libRef }) {
     }
 
     return (
-        <Link
-            href={{
-                pathname: '/details',
-            }}
-        >
-            <div className="flex flex-row flex-wrap">
-                <div className="flex max-w-xs w-full bg-white shadow-md rounded-lg overflow-hidden mx-auto max-h-full m-4">
-                    <div className="overflow-hidden rounded-xl relative transform hover:-translate-y-2 transition ease-in-out duration-500 shadow-lg hover:shadow-2xl movie-item text-white movie-card">
-                        <div className="absolute inset-0 z-10 transition duration-300 ease-in-out bg-gradient-to-t from-black via-runik-neutral-dark to-transparent" />
-                        <div className="relative cursor-pointer group z-10 pl-3.5 pr-6 pt-6 space-y-20">
-                            <div className="flex w-full justify-end">
-                                <button
-                                    type="button"
-                                    className="hover:bg-runik-primary-light rounded-full"
-                                >
-                                    <PlusCircle
-                                        url={result.url}
-                                        onclick={() => addToLibrary()}
-                                    />
-                                </button>
-                            </div>
+        <div className="flex flex-row flex-wrap">
+            <div className="flex max-w-xs w-full bg-white shadow-md rounded-lg overflow-hidden mx-auto max-h-full m-4">
+                <div className="overflow-hidden rounded-xl relative transform hover:-translate-y-2 transition ease-in-out duration-500 shadow-lg hover:shadow-2xl movie-item text-white movie-card">
+                    <div className="absolute inset-0 z-10 transition duration-300 ease-in-out bg-gradient-to-t from-black via-runik-neutral-dark to-transparent" />
+                    <div className="relative cursor-pointer group z-10 pl-3.5 pr-6 pt-6 space-y-20">
+                        <div className="flex w-full justify-end">
+                            <button
+                                type="button"
+                                className="hover:bg-runik-primary-light rounded-full"
+                            >
+                                <PlusCircle
+                                    url={result.url}
+                                    onclick={() => addToLibrary()}
+                                />
+                            </button>
+                        </div>
+                        <Link
+                            href={{
+                                pathname: '/details',
+                            }}
+                        >
                             <div className="poster__info align-self-end w-full h-80">
                                 <div className="h-32 m-6" />
                                 <div className="space-y-6 detail_info">
@@ -76,15 +76,15 @@ export default function ResultsCard({ libRef }) {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <img
-                            className="absolute inset-0 transform w-full -translate-y-4"
-                            src={result.thumbnail}
-                            alt="dictionaryImage"
-                        />
+                        </Link>
                     </div>
+                    <img
+                        className="absolute inset-0 transform w-full -translate-y-4"
+                        src={result.thumbnail}
+                        alt="dictionaryImage"
+                    />
                 </div>
             </div>
-        </Link>
+        </div>
     );
 }
