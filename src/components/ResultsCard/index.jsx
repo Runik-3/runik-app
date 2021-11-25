fd626d83e9e0c14fb232e9bc085e4c67e08e6395
dawsonGosse <dgosse5@my.bcit.ca>
2021-11-24 22:27:00 -0800

    Fixed card gradient and animation. added bigger plus button

--- src/components/ResultsCard/index.jsx
+++ src/components/ResultsCard/index.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 // import { add } from 'winston';
 // import AddButton from '../Icons/AddButton';
 import { useContext } from 'react';
-import PlusCircle from '../Icons/PlusCircle';
+import PlusCircleLarge from '../PlusCircleLarge';
 import { LibraryContext } from '../../context/libraryContext';
 import booksList from '../../data/booksList.json';
 
@@ -36,16 +36,16 @@ export default function ResultsCard({ libRef }) {
 
     return (
         <div className="flex flex-row flex-wrap">
-            <div className="flex max-w-xs w-full bg-white shadow-md rounded-lg overflow-hidden mx-auto max-h-full m-4">
-                <div className="overflow-hidden rounded-xl relative transform hover:-translate-y-2 transition ease-in-out duration-500 shadow-lg hover:shadow-2xl movie-item text-white movie-card">
-                    <div className="absolute inset-0 z-10 transition duration-300 ease-in-out bg-gradient-to-t from-black via-runik-neutral-dark to-transparent" />
+            <div className="flex max-w-xs w-full bg-white shadow-md rounded-lg overflow-hidden mx-auto max-h-full m-4 transform hover:-translate-y-2 transition ease-in-out duration-500 shadow-lg hover:shadow-2xl">
+                <div className="overflow-hidden rounded-xl relative movie-item text-white movie-card">
+                    <div className="absolute inset-0 top-24 z-10 transition duration-300 ease-in-out bg-gradient-to-t from-black via-runik-neutral-dark to-transparent h-96" />
                     <div className="relative cursor-pointer group z-10 pl-3.5 pr-6 pt-6 space-y-20">
                         <div className="flex w-full justify-end">
                             <button
                                 type="button"
                                 className="hover:bg-runik-primary-light rounded-full"
                             >
-                                <PlusCircle
+                                <PlusCircleLarge
                                     url={result.url}
                                     onclick={() => addToLibrary()}
                                 />
@@ -61,7 +61,7 @@ export default function ResultsCard({ libRef }) {
                                 <div className="space-y-6 detail_info">
                                     <div className="flex flex-col space-y-2 inner">
                                         <h3
-                                            className="text-3xl font-bold text-white"
+                                            className="text-3xl font-semibold text-white"
                                             data-unsp-sanitized="clean"
                                         >
                                             {result.title}
@@ -70,7 +70,7 @@ export default function ResultsCard({ libRef }) {
                                     <div className="flex flex-col overview">
                                         <div className="flex flex-col" />
 
-                                        <p className="text-sm text-gray-100">
+                                        <p className="text-sm font-medium text-gray-100">
                                             {trim(result.description, 100)}
                                         </p>
                                     </div>
