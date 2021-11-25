7e83317e5e92648d27052a60823ac94d19d97608
EliasMatteo <eflamencosolito@my.bcit.ca>
2021-11-25 06:07:15 -0800

    Adjusted RunikIcon padding, made simple Footer on hover
    Adjusted RunikIcon to have matching padding as its opposite icon on the NavBar(LibraryIcon), Created simple Hover underlining for the clickable links in the footer(Temp effect will most likely change to some more "dynamic")

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,15 +4,15 @@ import Link from 'next/link';
 const Footer = () => {
     return (
         <div className="w-full flex justify-between p-3 border-runik-neutral-gray border-t text-lg font-normal text-runik-neutral-med">
-            <div className="flex flex-row justify-evenly ">
+            <div className="flex flex-row justify-evenly cursor-pointer">
                 <Link href="/">
-                    <p className="pr-5 pl-5">Home</p>
+                    <p className="pr-5 pl-5 hover:underline">Home</p>
                 </Link>
                 <Link href="about">
-                    <p className="pr-5 pl-5">About</p>
+                    <p className="pr-5 pl-5 hover:underline">About</p>
                 </Link>
                 <Link href="support">
-                    <p className="pr-5 pl-5">Support</p>
+                    <p className="pr-5 pl-5 hover:underline">Support</p>
                 </Link>
             </div>
             <div>
