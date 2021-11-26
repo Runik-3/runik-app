df876cff343582b3bdf4cc21815276e3bb9c371a
EliasMatteo <eflamencosolito@my.bcit.ca>
2021-11-25 23:16:25 -0800

    added hover effect to remove dictionary icon and added padding to NavBar/ restyled results page
    hover effect of red added to remove button in library pull out, also added small amount of padding to NavBar so it closer resembles figma mock up, restyled results page all but done but still missing top header title for page. its been started but still needs to be done up. also SearchBar comp is still bugging out when used in top NavBar

--- src/pages/results.jsx
+++ src/pages/results.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import Fuse from 'fuse.js';
 import ResultsCard from '../components/ResultsCard';
 import NavBar from '../components/NavBar';
-import ArrowBackIcon from '../components/Icons/ArrowBackIcon/Index';
 import booksList from '../data/booksList.json';
 import Footer from '../components/Footer';
 
@@ -27,14 +26,14 @@ export default function Results() {
     }, [router.isReady]);
 
     return (
-        <div className="flex justify-center w-full min-h-screen flex-col items-start pl-14 bg-gradient-to-b from-runik-neutral-light to-runik-background-blue">
+        <div className="flex flex-col justify-center items-center w-full min-h-screen bg-gradient-to-b from-runik-neutral-light to-runik-background-blue">
             {/* The NavBar has replaced the original standalone comps, the sizing and fitting is off and needs to be adjusted accordingly, the props attached to NavBar are for the icons that need to switch visibility depending on page */}
-            <div className="flex py-5 w-11/12 max-w-8xl justify-start items-start">
+            <div className="flex pb-5 w-full max-w-8xl justify-start items-start">
                 <NavBar aDisplay="hidden" rDisplay="flex" />
             </div>
             {/* this is new and needs to be here so user can go back to search page from results page, please make it fit correctly within the page */}
-            <div className="pt-8 flex justify-start items-start max-w-full w-11/12">
-                <ArrowBackIcon path="/" />
+            <div className="border-b border-[#C4C4C4]"> {/* YOURE HERE MATTEO */}
+                <p className="font-spartan font-semibold text-2xl ">Results</p>
             </div>
             <div className="w-11/12 flex justify-center items-center">
                 <div className="flex w-11/12 max-w-6xl justify-evenly items-center py-16 flex-wrap">
