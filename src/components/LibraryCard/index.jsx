13bf266bfc7880a76de3c9a97b9bbf868453b8b8
matteomiceli <omatteomiceli@gmail.com>
2021-11-26 21:35:06 -0800

    language selectors added to library cards

--- src/components/LibraryCard/index.jsx
+++ src/components/LibraryCard/index.jsx
@@ -7,17 +7,56 @@ import MinusCircle from '../Icons/MinusCircle';
 // eslint-disable-next-line react/prop-types
 const LibraryCard = ({ libRef }) => {
     const [, , removeReference] = useContext(LibraryContext);
+
     function removeCard(url) {
         removeReference(url);
     }
 
+    function handleChange(e) {
+        console.log(e.currentTarget.value);
+    }
+
     return (
         <div className="transition-all">
-            <div className="overflow-hidden flex p-3 pb-8 pt-2 shadow-lg rounded-lg w-80 h-20 bg-gradient-to-r from-runik-neutral-dark to-black justify-between mb-2">
-                <div className="text-lg text-left align-text-top text-white font-bold font-spartan ">
-                    {libRef.title}
+            <div className="overflow-hidden flex flex-col p-3 pb-8 pt-2 shadow-lg rounded-lg w-80 h-20 bg-gradient-to-r from-runik-neutral-dark to-black justify-between mb-2">
+                {' '}
+                <div className="flex justify-between w-full">
+                    <div className="text-lg text-left align-text-top text-white font-bold font-spartan ">
+                        {libRef.title}
+                    </div>
+                    <MinusCircle onclick={() => removeCard(libRef.url)} />
                 </div>
-                <MinusCircle onclick={() => removeCard(libRef.url)} />
+                <div className="">
+                    {libRef.languages.length !== 0 ? (
+                        <select
+                            onClick={(e) => handleChange(e)}
+                            className="bg-transparent text-white font-bold pr-8 border-1 border-white rounded-md p-0.5 pl-1"
+                            name="languages"
+                            id="languages"
+                        >
+                            <option
+                                defaultValue
+                                value="en"
+                                className="bg-black text-white font-bold"
+                            >
+                                en
+                            </option>
+                            {libRef.languages.map((lang) => {
+                                return (
+                                    <option
+                                        key={lang}
+                                        className="bg-black text-white font-bold"
+                                        value={lang}
+                                    >
+                                        {lang}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                    ) : (
+                        <p className="text-gray-300 font-bold pl-1.5">en</p>
+                    )}
+                </div>
                 {/* <img
                     className="absolute transform w-full -translate-y-4"
                     src={libRef.thumbnail}
