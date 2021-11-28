dc1221c193251b239dcef824689cc67a25f7548b
EliasMatteo <eflamencosolito@my.bcit.ca>
2021-11-27 20:14:14 -0800

    pls fix it self

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -6,7 +6,6 @@ const Button = ({ size, text, onclick }) => {
             type="button"
             className={`flex ${
                 size === 'md' ? 'py-6' : 'py-4'
- fix/finishedResultsPage/MorePaddingAdjust/DetailsPage
             } px-14 border-2 border-runik-neutral-dark rounded-xl font-spartan font-semibold transition-ease-in-out duration-500 hover:bg-runik-primary-light`}
             onClick={onclick}
         >
