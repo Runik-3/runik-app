c34ef57aca813a0d2681a0391eea7677d4c6c64a
iflinda <informlinda@gmail.com>
2021-11-29 18:29:15 -0800

    Fixed how parameter is passed between components in the detail page

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -5,10 +5,17 @@ import DetailsCard from '../components/DetailsCard';
 import NavBar from '../components/NavBar';
 import ArrowBackIcon from '../components/Icons/ArrowBackIcon/Index';
 import Footer from '../components/Footer';
+import booksList from '../data/booksList.json';
 
 export default function Details() {
     const router = useRouter();
     const result = router.query;
+
+    const book = booksList.filter((checkBook) => {
+        return checkBook.url === result.url;
+    });
+    const { url, title, author, description, thumbnail, category, languages } =
+        book[0];
     return (
         <div className="flex flex-col items-center relative z-0 overflow-visible w-full min-h-screen bg-gradient-to-b from-runik-neutral-light to-runik-background-blue">
             <div className="flex pb-5 w-full max-w-8xl justify-start items-start">
@@ -24,11 +31,13 @@ export default function Details() {
             </div>
             <div className="flex w-11/12 max-w-6xl justify-center items-center py-9">
                 <DetailsCard
-                    url={result.url}
-                    title={result.title}
-                    author={result.author}
-                    description={result.description}
-                    thumbnail={result.thumbnail}
+                    url={url}
+                    title={title}
+                    author={author}
+                    description={description}
+                    thumbnail={thumbnail}
+                    category={category}
+                    languages={languages}
                 />
             </div>
             {/* <div className="font-spartan font-medium text-2xl pt-16">
