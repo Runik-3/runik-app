a00e92da541da44893c93fbb5e9aaced1609f3cb
Matteo Miceli <omatteomiceli@gmail.com>
2021-12-03 01:15:50 -0800

    button styling

--- src/components/HeadlessSlideOver/index.jsx
+++ src/components/HeadlessSlideOver/index.jsx
@@ -247,10 +247,10 @@ export default function HeadlessSlideOver({ open, setOpen }) {
                         <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex sm:pl-16">
                             <Transition.Child
                                 as={Fragment}
-                                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                                enter="transform transition ease-in-out duration-200"
                                 enterFrom="translate-x-full"
                                 enterTo="translate-x-0"
-                                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                                leave="transform transition ease-in-out duration-200"
                                 leaveFrom="translate-x-0"
                                 leaveTo="translate-x-full"
                             >
@@ -345,7 +345,7 @@ export default function HeadlessSlideOver({ open, setOpen }) {
                                                                         false
                                                                     );
                                                                 }}
-                                                                className="flex-1 bg-runik-primary-dark bg-opacity-75 hover:bg-opacity-100 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white"
+                                                                className="flex-1 bg-white text-runik-neutral-dark hover:bg-opacity-100 py-2 px-4 border-2 border-runik-neutral-dark rounded-md hover:shadow-lg hover:bg-runik-primary-med hover:border-transparent duration-200 font-medium"
                                                             >
                                                                 Install All
                                                             </button>
