24337731221eec0b14f5e63fcf19479aabd49fcf
Matteo Miceli <matteo@node-1w7jr9qvb31xz7li7rft5labu.ipv6.telus.net>
2021-12-14 15:49:31 -0800

    add a software exception to only accept references from prod and staging for the s3UrlUpload function

--- src/pages/api/s3.js
+++ src/pages/api/s3.js
@@ -1,7 +1,30 @@
 import generateS3Url from './s3Setup';
 
+const allowedReferers = [
+    'https://runik.app/',
+    'https://runik-app-mauve.vercel.app/',
+    'http://localhost:8080/',
+];
+
 export default async function handler(req, res) {
-    const { target, name, lang } = req.query;
-    const url = await generateS3Url(target, name, lang);
-    res.send({ url });
+    const { referer } = req.headers;
+    console.log(referer);
+    let sameOrigin = false;
+    if (referer) {
+        allowedReferers.forEach((allowed) => {
+            if (allowed === referer) {
+                sameOrigin = true;
+            }
+        });
+    }
+    if (sameOrigin) {
+        const { target, name, lang } = req.query;
+        const url = await generateS3Url(target, name, lang);
+        res.send({ url });
+    } else {
+        res.status(403).json({
+            success: false,
+            message: 'Request not from same origin.',
+        });
+    }
 }
